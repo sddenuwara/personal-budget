348e4a74f5377c032f8161c5a87ef568a8da6406
Sach Denuwara <sddenuwara@gmail.com>
2023-09-17 23:02:23 -0400

    Loading data from JSON file

--- server.js
+++ server.js
@@ -4,28 +4,12 @@ const port = 3000;
 
 app.use('/', express.static('public'));
 
-const budget = {
-    myBudget: [
-        {
-            title: 'Eat out',
-            budget: 30
-        },
-        {
-            title: 'Rent',
-            budget: 350
-        },
-        {
-            title: 'Grocery',
-            budget: 90
-        }
-    ]
-};
-
 app.get('/hello', (req, res) => {
     res.send('Hello World!');
 });
 
 app.get('/budget', (req, res) => {
+    const budget = require('./budget-data.json');
     res.json(budget);
 });
 
