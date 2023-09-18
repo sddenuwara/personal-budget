ea55365e9aaa57a6f1c24dd2ec4ff09382beb009
Sach Denuwara <sddenuwara@gmail.com>
2023-09-17 21:39:18 -0400

    Added server, Axios, and ChartJS

--- server.js
+++ server.js
@@ -1,34 +1,34 @@
-// Budget API
-
 const express = require('express');
-const cors = require('cors');
 const app = express();
 const port = 3000;
 
-app.use(cors());
+app.use('/', express.static('public'));
 
 const budget = {
     myBudget: [
         {
             title: 'Eat out',
-            budget: 25
+            budget: 30
         },
         {
             title: 'Rent',
-            budget: 275
+            budget: 350
         },
         {
             title: 'Grocery',
-            budget: 110
-        },
+            budget: 90
+        }
     ]
 };
 
+app.get('/hello', (req, res) => {
+    res.send('Hello World!');
+});
 
 app.get('/budget', (req, res) => {
     res.json(budget);
 });
 
 app.listen(port, () => {
-    console.log(`API served at http://localhost:${port}`);
+    console.log(`Example app listening at http://localhost:${port}`);
 });
