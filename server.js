756e2f4c1ae61f33a3a2b66cf1f3d5bb7daaa04e
Sach Denuwara <sddenuwara@gmail.com>
2023-09-21 12:49:19 -0400

    Added CORS

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const port = 3000;
 
-app.use('/', express.static('public'));
+app.use(cors());
 
 app.get('/hello', (req, res) => {
     res.send('Hello World!');
@@ -14,5 +15,5 @@ app.get('/budget', (req, res) => {
 });
 
 app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`);
+    console.log(`API served at http://localhost:${port}`);
 });
